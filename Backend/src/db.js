bd6c44e1e33cc15982653dcc18e8313d19a223b1
Haila-Atsibha <hmamulat@gmail.com>
2026-05-17 21:12:39 +0300

    some fixes

--- Backend/src/db.js
+++ Backend/src/db.js
@@ -4,7 +4,11 @@ const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
-    }
+    },
+    keepAlive: true,
+    keepAliveInitialDelayMillis: 10000,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 10000,
 });
 
 module.exports = pool;
